c560d7e77de5ae74530e3c3af1efaf04c82efd91
menjin <menjin728@gmail.com>
2024-07-13 19:00:04 +0800

    SanYuanYunSuanFu

---
+++ src/menjinyan/java/Demo09If.java
@@ -0,0 +1,24 @@
+package src.menjinyan.java;
+
+public class Demo09If {
+    public static void main(String[] args) {
+        //if语句：
+        //需求：键盘录入女婿的酒量，如果大于2斤，老丈人给出回应，反之不回应
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("请输入女婿的酒量");
+//        int wine=sc.nextInt();
+//        if(wine>2){
+//            System.out.println("小伙子，不错");
+//        }
+
+        //练习1：
+        //小红：如果你这次考试是第一，我就做你的女朋友
+        //1、定义变量记录小鸣的成绩
+//        int ranking=1;
+//        //2、对小鸣的名字进行判断
+//        if(ranking==1){
+//            System.out.println("小红变为小明的女朋友");
+//        }
+
+    }
+}
