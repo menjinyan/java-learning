fbb550e89fb34e3fa9ca867aa3e49480b98fedd6
menjin <menjin728@gmail.com>
2024-07-13 18:56:56 +0800

    SanYuanYunSuanFu

---
+++ src/menjinyan/java/Demo08SanYuanYunSuanFu.java
@@ -0,0 +1,35 @@
+package src.menjinyan.java;
+
+public class Demo08SanYuanYunSuanFu {
+    public static void main(String[] args) {
+
+        //三元运算符
+        //练习1
+        //需求：动物园里有两只老虎，体重分别为通过键盘录入获得，
+        //请用程序实现判断两只老虎的体重是否相同。
+
+        //分析：
+        //1、键盘录入两只老虎的体重
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("请输入第一只老虎的体重");
+//        int weight1 = sc.nextInt();
+//        System.out.println("请输入第二只老虎的体重");
+//        int weight2 = sc.nextInt();
+//       String result = weight1==weight2 ? "相同" : "不同";
+//       System.out.println(result);
+
+        //练习2
+        //需求：一座寺庙里住着三个和尚，已知的是150cm,120cm,165cm.
+        //请用程序实现获取三个和尚的最高身高
+
+        //1、定义三个变量记录的三个和尚的身高
+//        int height1=150;
+//        int height2=200;
+//        int height3=165;
+//        //2.拿着第一个和尚和第二个和尚进行比较
+//        //拿着结果和第三个和尚进行比较即可
+//        int temp=height1>height2 ? height1:height2;
+//        int max=temp>height3 ? temp:height3;
+//        System.out.println(max);
+    }
+}
