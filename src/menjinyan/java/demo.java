6bbc3b7fe556c45d21eaaff6045ed403b5b40bcd
menjin <menjin728@gmail.com>
2024-07-13 19:10:38 +0800

    if

--- src/menjinyan/java/demo.java
+++ src/menjinyan/java/demo.java
@@ -2,78 +2,8 @@ package src.menjinyan.java;
 
 public class demo {
     public static void main(String[] args) {
-        //逻辑运算符
 
-        //1、& 并且
-        //运算逻辑：两边都为真，结果才是真
-//        System.out.println(true & true);
-//        System.out.println(true & false);
-//        System.out.println(false & false);
-//        System.out.println(false & true);
-        //2 | 或者
-        //运算逻辑：两边都为假，结果才是假，只要有一个为真结果就是真
-//        System.out.println(true | true);
-//        System.out.println(true | false);
-//        System.out.println(false | false);
-//        System.out.println(false | true);
 
-        //3、^异或
-        //运算逻辑：相同为false 不相同为true
-//        System.out.println(true ^ false);
-//        System.out.println(true ^ true);
-//        System.out.println(true ^ false);
-//        System.out.println(false ^ true);
-
-        //4、！逻辑非   取反
-//        System.out.println(!false);
-//        System.out.println(!true);
-
-        //5、练习  数字6
-        //需求：数字6是一个真正伟大的数字，键盘录入两个整数
-        //如果其中一个为6，最终输出结果是true
-        //如果他们的和为6的倍数，最终输出为true,其他情况都是false.
-        //分析
-        //1、键盘输入两个整数
-        //变量a 变量b
-        //2、a == 6 || b == 6 (a+b)%6 == 0  (如果满足其中一个那么就可以输出为true)
-        //键盘录入两个整数
-//        Scanner sc = new Scanner(System.in);
-//        System.out.println("请输入一个整数");
-//        int number1 = sc.nextInt();
-//        System.out.println("请输入第二个整数");
-//        int number2 = sc.nextInt();
-////可以短路逻辑运算符去连接三个判断
-//        boolean result =number1==6 || number2==6 || (number1+number2) % 6 ==0;
-//        System.out.println(result);
-
-        //三元运算符
-        //练习1
-        //需求：动物园里有两只老虎，体重分别为通过键盘录入获得，
-        //请用程序实现判断两只老虎的体重是否相同。
-
-        //分析：
-        //1、键盘录入两只老虎的体重
-//        Scanner sc = new Scanner(System.in);
-//        System.out.println("请输入第一只老虎的体重");
-//        int weight1 = sc.nextInt();
-//        System.out.println("请输入第二只老虎的体重");
-//        int weight2 = sc.nextInt();
-//       String result = weight1==weight2 ? "相同" : "不同";
-//       System.out.println(result);
-
-        //练习2
-        //需求：一座寺庙里住着三个和尚，已知的是150cm,120cm,165cm.
-        //请用程序实现获取三个和尚的最高身高
-
-        //1、定义三个变量记录的三个和尚的身高
-//        int height1=150;
-//        int height2=200;
-//        int height3=165;
-//        //2.拿着第一个和尚和第二个和尚进行比较
-//        //拿着结果和第三个和尚进行比较即可
-//        int temp=height1>height2 ? height1:height2;
-//        int max=temp>height3 ? temp:height3;
-//        System.out.println(max);
 
         //if语句：
         //需求：键盘录入女婿的酒量，如果大于2斤，老丈人给出回应，反之不回应
@@ -114,70 +44,8 @@ public class demo {
 //        }
 
 
-//        //if else语句
-//        //需求：键盘录入一个整数，表示身上的钱
-//        //如果大于100，吃大餐，小于100吃经济实惠的小吃
-//        Scanner sc = new Scanner(System.in);
-//        System.out.println("请输入钱数");
-//        int money = sc.nextInt();
-//        if (money >= 100) {
-//            System.out.println("吃大餐");
-//            }
-//        else{
-//            System.out.println("吃经济实惠的小吃");
-//        }
-
-        //if elae 语句的练习 1
-        //商品付款  （在实际开发中，如果要根据两种不同的情况来执行不同的代码，就需要用到if的第二种格式）
-        //需求：
-        //假设，用户在超市实际购买，商品的总价为600元，键盘录入一个整数表示实际用户支付的钱，如果付款大于等于600，表示付款成功，否则失败
-//        Scanner sc = new Scanner(System.in);
-//        System.out.println("录入一个整数表示实际支付的钱");
-//        int money = sc.nextInt();
-//        if(money>=600){
-//            System.out.println("付款成功");
-//        }else{
-//            System.out.println("付款失败");
-//        }
-
-        //if elae 语句练习 2
-        //电影院卖票为1~100 奇数坐在左边偶数坐在右边
-//        Scanner sc = new Scanner(System.in);
-//        System.out.println("请输入一个票号");
-//        int ticket=sc.nextInt();
-//        if(ticket >=0 && ticket<=100){
 //
-//                if(ticket % 2 ==1){
-//                    System.out.println("做左边");
-//                }
-//                else{
-//                    System.out.println("坐右边");
-//                }
-//        }
 
-
-        //if 语句的第三种格式练习
-        //根据不同的分数送不同的礼物
-        //如果是95~100分，送自行车
-        //如果是90~94分 游乐场一天
-        //如果是80~89分 变形金刚
-        //如果是80分，打一顿
-//        Scanner sc = new Scanner(System.in);
-//        System.out.println("请输入成绩");
-//        int score = sc.nextInt();
-//        if(score >0 && score <=100) {
-//            if (score >= 95 && score <= 100) {
-//                System.out.println("自行车");
-//            }else if (score >= 90 && score <= 94) {
-//                System.out.println("游乐场");
-//            }else if (score > 80 && score <= 89) {
-//                System.out.println("变形金刚");
-//            }else {
-//                System.out.println("打一顿");
-//            }
-//        }else{
-//            System.out.println("录入的成绩不合规范");
-//        }
 
         //练习3：
         //需求：商场开启会员制，假设商品的总价为1000，会员1级打九折
