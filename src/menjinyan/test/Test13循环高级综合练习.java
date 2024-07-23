720839b2ca06b838dbc10fadfcfac2d838e8f6b9
menjin <menjin728@gmail.com>
2024-07-23 22:25:08 +0800

    test

---
+++ src/menjinyan/test/Test13循环高级综合练习.java
@@ -0,0 +1,23 @@
+package src.menjinyan.test;
+
+public class Test13循环高级综合练习 {
+    public static void main(String[] args) {
+        //for格式的无限循环
+        /*for (;;){
+            System.out.println("学习");
+        }*/
+
+        //while格式的无限循环
+        /*while(true){
+            System.out.println("学习");
+        }*/
+
+        //注意事项：
+        //无限循环的下面不能再写其他代码了，因为循环永远停不下来，下面的代码永远执行不到
+        while(true){
+            System.out.println("给女神表白");
+
+        }
+       // System.out.println("女神答应我了"); 这一步就是在无限循环下面写的代码，执行不到
+    }
+}
