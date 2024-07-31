81340f250d0545d041d0ee86444dcde262e9eec2
menjin <menjin728@gmail.com>
2024-07-31 22:23:44 +0800

    Demo

---
+++ src/menjinyan/java/Demo14数值的地址值.java
@@ -0,0 +1,17 @@
+package src.menjinyan.java;
+
+public class Demo14数值的地址值 {
+    public static void main(String[] args) {
+       /* Demo14数组的地址值
+                int []arr={1,2,3,4};
+        System.out.println(arr);  [I@b4c966a(这个就数组的地址值)*/
+
+        //扩展：
+        //解释一下地址值的格式含义  [D@776ec8df
+        //[ :表示当前是一个数组
+        //D:表示当前数组里面的元素都是double类型的
+        //@: 表示一个建个符号。（固定格式）
+        //776ec8df:才是数组真正的地址值（十六进制）
+        //平时我们习惯性的会把这个整体叫做数组的地址值。
+    }
+}
