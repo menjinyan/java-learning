99d8e914e800f8f516ae8196cc99365d938d5d9d
menjin <menjin728@gmail.com>
2024-07-31 21:46:37 +0800

    Demo

---
+++ src/menjinyan/java/Demo13数组.java
@@ -0,0 +1,18 @@
+package src.menjinyan.java;
+
+public class Demo13数组 {
+    public static void main(String[] args) {
+        //需求1：定义数组存储5个学生的年龄
+        int[] arr1 = new int[]{11, 12, 13, 14};
+        //简化格式
+        int[] arr2 = {11, 12, 13, 14};
+
+        //需求2：定义数组存储3个学生的姓名
+        String[] arr3 = new String[]{"zhangsan", "lisi"};
+        String[] arr4 = {"zhangsan", "lisi"};
+
+        //需求3：定义数组存储4个学生的身高
+        double[] arr5 = new double[]{1.93, 1.75, 1.84};
+        double[] arr6 = {1.93, 1.75, 1.84};
+    }
+}
