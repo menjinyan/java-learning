f038e6506e7f9b5041224210148025bc38d1a570
menjinyan <menjin728@gmail.com>
2024-10-11 15:29:58 +0800

    提交

---
+++ Topics/数字加密pro.java
@@ -0,0 +1,43 @@
+package Topics;
+
+public class 数字加密pro {
+    public static void main(String[] args) {
+        //需求:
+        //把整数上的每一位都添加到数组当中
+        //反向推到
+
+        //1、计算出数组的长度
+        int number = 12345;
+        //定义一个变量临时记录Number的值，九十为了第三步的时候再次使用
+        int temp = number;
+//定义一个变量进行统计
+        int count = 0;
+        while (number != 0) {
+//每一次循环去掉右边的一个数字
+            number = number / 10;
+            System.out.println(number);
+            //去掉一位计数器就自增一次
+            count++;
+        }
+        System.out.println(count);
+        //2、定义一个数组
+        //动态初始化
+        int[] arr = new int[count];
+
+        //3、把整数上的每一位都添加到数组当中
+        int index = arr.length - 1;
+        while (temp != 0) {
+            //获取temp里面的每一位数组
+            int ge = temp % 10;
+            //再去掉右边的那位数字
+            temp = temp / 10;
+            //把当前获取到的个位添加到数组当中
+            arr[index++] = ge;
+            index--;
+        }
+        //验证结果 1 2 3 4 5
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + " ");
+        }
+    }
+}
---
+++ Topics/数字解密.java
@@ -0,0 +1,41 @@
+package Topics;
+
+public class 数字解密 {
+    public static void main(String[] args) {
+        /*某系统的数字密码(大于0),比如1983，采用加密的方式进行传输，
+        规则如下:
+        每位数加上5，
+        在对余求10.最后将所有的数字反转
+        得到一串新的数字，
+        按照以往规则进行解密
+        比如1983加密之后变成8364，解密之后变成1983*/
+
+        //1、定义数组记录解密之后的结果
+        int[] arr = {8, 3, 4, 6};
+        //2、反过来推
+        for (int i = 0, j = arr.length - 1; i < j; i++, j--) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        //3、由于加密是通过对10取余的方式进行获取的
+        //所以在解密的时候就需要判断 0~4之间+10 5~9之间数字不变
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] >= 0 && arr[i] <= 4) {
+                arr[i] = arr[i] + 10;
+            }
+        }
+        //4、每一位减5
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = arr[i] - 5;
+        }
+        //5、获取数组里面的每一位数字拼接成最终的结果
+        int number = 0;
+        for (int i = 0; i < arr.length; i++) {
+            number = number * 10 + arr[i];
+        }
+        System.out.println(number);
+
+    }
+}
