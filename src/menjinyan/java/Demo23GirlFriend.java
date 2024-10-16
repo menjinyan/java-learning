8baaf5c21afd1c2972386ec739c1630da8c67e21
menjinyan <menjin728@gmail.com>
2024-10-16 20:01:49 +0800

    提交

---
+++ src/menjinyan/java/Demo23GirlFriend.java
@@ -0,0 +1,19 @@
+package src.menjinyan.java;
+
+public class Demo23GirlFriend {
+    //属性
+    String name;
+    int age;
+    String gender;
+    String eat;
+    String sleep;
+
+    //行为
+    public void sleep() {
+        System.out.println("女朋友在睡觉");
+    }
+
+    public void eat() {
+        System.out.println("女朋友在吃饭");
+    }
+}
