2805c8eb3d67f83eb813854da56fefddd8ec8250
menjinyan <menjin728@gmail.com>
2024-10-18 15:48:13 +0800

    submint

---
+++ src/构造方法/StudentTest.java
@@ -0,0 +1,13 @@
+package src.构造方法;
+
+public class StudentTest {
+    public static void main(String[] args) {
+        //创建对象
+        //Student s1 = new Student();//小括号里什么都没有是调用的空参构造
+
+       /* Student s1 = new Student("menjinyan", 22);
+        System.out.println(s1.getName());
+        System.out.println(s1.getAge());*/
+
+    }
+}
