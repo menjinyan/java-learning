fcaab7cdd021caccd4bbb7afb29f9980e45e9557
menjinyan <menjin728@gmail.com>
2024-10-20 20:35:55 +0800

    submint

---
+++ src/对象数组练习/Shopping.java
@@ -0,0 +1,50 @@
+package src.对象数组练习;
+
+public class Shopping {
+    private String id;
+    private String name;
+    public double price;
+    public int count;
+
+    public Shopping() {
+    }
+
+    public Shopping(String id, String name, double price, int count) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.count = count;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
