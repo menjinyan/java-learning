4709fe824d2180ac94ff8180f6d89f8f7357592a
menjinyan <menjin728@gmail.com>
2024-11-02 17:15:59 +0800

    Submint

--- src/StringBuilder/StringBuilderDemo04对称字符串.java
+++ src/StringBuilder/StringBuilderDemo04对称字符串.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 
 public class StringBuilderDemo04对称字符串 {
     public static void main(String[] args) {
+        //使用StringBuilder的场景:
+        //1、字符串的拼接
+        //2、字符串的反转
+
         //需求:
         //键盘接受一个字符串，程序判断出该字符串是否是对称字符串，并在控制台打印是或不是
         //对称字符串:123321、111
