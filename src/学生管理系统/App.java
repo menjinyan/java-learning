96942825144e8e286ebed38137020e6391eb216f
menjinyan <menjin728@gmail.com>
2024-11-27 16:58:41 +0800

    Submint

--- src/学生管理系统/App.java
+++ src/学生管理系统/App.java
@@ -1,9 +1,10 @@
 package src.学生管理系统;
 
-import src.集合.User;
+import src.学生管理系统.User;
 
 import javax.swing.*;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class App {
@@ -35,11 +36,13 @@ public class App {
         System.out.println("注册");
         //把用户添加到集合当中
         User user = new User();
+
         //1、键盘录入用户名
         Scanner sc = new Scanner(System.in);
+        String username;
         while (true) {
             System.out.println("请输入用户名");
-            String username = sc.next();
+            username = sc.next();
 
             //定义一个方法
             boolean flag1 = checkUsername(username);
@@ -62,12 +65,13 @@ public class App {
 
         //2、键盘录入密码
         //密码键盘输入两次，两次一致才可以注册
+        String password;
         while (true) {
             System.out.println("请输入需要注册的密码");
-            String Password = sc.next();
+            password = sc.next();
             System.out.println("请再次输入需要注册的密码");
             String againPassword = sc.next();
-            if (!Password.equals(againPassword)) {
+            if (!password.equals(againPassword)) {
                 System.out.println("两次密码输入的不一致，请重新输入");
                 continue;
             } else {
@@ -78,10 +82,78 @@ public class App {
 
         //3、键盘录入身份信息
         //长度为18位，不能是0开头的前17位必须是数字，最后一位可以是数字也可以是大写的X或者小写的x
-        System.out.println("请输入正确的身份证号码");
-        String personID = sc.next();
-        boolean flage = checkpersonID(personID);
+        String personID;
+        while (true) {
+            System.out.println("请输入正确的身份证号码");
+            personID = sc.next();
+            boolean flag = checkpersonID(personID);
+            if (flag) {
+                System.out.println("身份证号码满足要求");
+                break;
+            } else {
+                System.out.println("身份证号码格式有误请重新输入");
+                continue;
+            }
+        }
+
+        //4、键盘录入手机号码
+        /*长度为11位
+          不能以0开头
+          必须都是数字*/
+        String phoneNumber;
+        while (true) {
+            System.out.println("请输入手机号码");
+            phoneNumber = sc.next();
+            boolean flag = checkPhoneNuber(phoneNumber);
+            if (flag) {
+                System.out.println("手机号码格式正确");
+                break;
+            } else {
+                System.out.println("手机号码格式有误，请出重新输入");
+                continue;
+            }
+        }
+
+        //用户名，密码，身份证号码，手机号码放到用户对象当中
+        User u = new User(username, password, personID, phoneNumber);
+
+        //把用户对象添加到集合里面
+        list.add(u);
+        System.out.println("注册成功");
+
+        //验证是否真的注册成功，要遍历集合
+        printList(list);
     }
+
+    private static void printList(ArrayList<User> list) {
+        for (int i = 0; i < list.size(); i++) {
+            //这里的i依旧表示索引
+            User user = list.get(i);
+            System.out.println(user.toString() + "," + user.getPassword() + "," + user.getPersonID() + "," + user.getPhoneNumber());
+
+        }
+    }
+
+    private static boolean checkPhoneNuber(String phoneNumber) {
+        //长度为11位
+        if (phoneNumber.length() != 11) {
+            return false;
+        }
+        //不能以0开头
+        if (phoneNumber.startsWith("0")) {
+            return false;
+        }
+        //必须都是数字
+        for (int i = 0; i < phoneNumber.length(); i++) {
+            char c = phoneNumber.charAt(i);
+            if (!(c >= '9' && c <= '9')) {
+                return false;
+            }
+        }
+//当循环结束之后，表示每一个字符都在0~9之间
+        return true;
+    }
+
 
     private static boolean checkpersonID(String personID) {
         //长度为18位
@@ -117,7 +189,7 @@ public class App {
         for (int i = 0; i < list.size(); i++) {
             //i在这里还是表示集合当中的每一个索引
             User user = list.get(i);//使用get方法拿到每一个用户
-            String rightUserNmae = user.getUesername();//表示集合里面的用户信息都是正确的意思
+            String rightUserNmae = user.getUsernamne();//表示集合里面的用户信息都是正确的意思
             if (username.equals(rightUserNmae)) {//拿着正确的用户名和正在查找的用户名来做一个比较
                 return true;
             }
@@ -148,17 +220,52 @@ public class App {
         for (int i = 0; i < username.length(); i++) {
             //i表示索引
             char ch = username.charAt(i);//通过usernam调用charAt的方法来获取每一个字符
-            //判断字符ch是不是在这里面呢  下面的方法就是当前的字符不在这三个范围之内就返回一个false (!就是取反的意思)
+            //判断字符ch是不是在这里面?  下面的方法就是当前的字符不在这三个范围之内就返回一个false (!就是取反的意思)
             if (!((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))) {
                 count++;
                 break;//当有一个满足就直接break
             }
         }
-        return count >= 0;
+        return count > 0;
     }
 
     private static void login(ArrayList<User> list) {
         System.out.println("登录");
+        Scanner sc = new Scanner(System.in);
 
+//输入用户名
+        System.out.println("请输入用户名");
+        String inputUsernam = sc.next();
+        //查找用户是否存在
+        User foundUser = null;
+        for (User user : list) {
+            if (user.getUsernamne().equals(inputUsernam)) {
+                foundUser = user;
+                break;
+            }
+        }
+        if (foundUser == null) {
+            System.out.println("用户名不存在，请先注册");
+            return;
+        }
+        //输入密码
+        System.out.println("请先输入密码");
+        String inputPassword = sc.next();
+        if (foundUser.getPassword().equals(inputPassword)) {
+            System.out.println("登录成功");
+        }else{
+            System.out.println("密码错误，请重新输入");
+        }
+    }
+    //生成验证码的方法，这里简单生成一个四位随机数字的验证码
+    private static String getCode(){
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 4; i++) {
+            sb.append(random.nextInt(10));
+        }
+        return sb.toString();
     }
 }
+
+
