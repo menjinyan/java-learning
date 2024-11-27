803713d847aff104cda2a37f9287f872be200f2f
menjinyan <menjin728@gmail.com>
2024-11-27 19:03:32 +0800

    Submint

---
+++ Static/src/StaticDemo01/Student.java
@@ -0,0 +1,48 @@
+package StaticDemo01;
+
+public class Student {
+    //需求:写一个JavaBean类来描述这个班级的学生
+    //属性:姓名、年龄、性别
+    //行为:学习
+    private String name;
+    private int age;
+    private String gender;
+
+    public Student() {
+    }
+
+    public Student(int age, String gender, String name) {
+        this.age = age;
+        this.gender = gender;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //行为
+    public void study() {
+        System.out.println(name + "正在学习");
+    }
+}
