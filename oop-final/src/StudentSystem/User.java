38004269d5feab1741b4e8773496315c0375b631
menjinyan <menjin728@gmail.com>
2025-01-24 13:30:28 +0800

    Submit

---
+++ oop-final/src/StudentSystem/User.java
@@ -0,0 +1,50 @@
+package StudentSystem;
+
+public class User {
+    private String usernamne;
+    private String password;
+    private String personID;
+    private String PhoneNumber;
+
+    public User() {
+    }
+
+    public User(String password, String personID, String phoneNumber, String usernamne) {
+        this.password = password;
+        this.personID = personID;
+        PhoneNumber = phoneNumber;
+        this.usernamne = usernamne;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPersonID() {
+        return personID;
+    }
+
+    public void setPersonID(String personID) {
+        this.personID = personID;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        PhoneNumber = phoneNumber;
+    }
+
+    public String getUsernamne() {
+        return usernamne;
+    }
+
+    public void setUsernamne(String usernamne) {
+        this.usernamne = usernamne;
+    }
+}
