17dc4135582038178f51d2eb4f82cbcbb7267c5e
menjinyan <menjin728@gmail.com>
2025-02-13 21:17:23 +0800

    Math

--- src/API/Math/MathDemo01.java
+++ src/API/Math/MathDemo01.java
@@ -3,11 +3,11 @@ package src.API.Math;
 public class MathDemo01 {
     /*
     public static int abs(int a) 获取参数绝对值
-public static double ceil(double a) 向上取整
-public static double floor(double a) 向下取整
-public static int round(float a) 四舍五入
-public static int max(int a,int b) 获取两个int值中的较大值
-public static double pow(double a,double b) 返回a的b次幂的值
-public static double random() 返回值为double的随机值，范围[0.0,1.0)
+    public static double ceil(double a) 向上取整
+    public static double floor(double a) 向下取整
+    public static int round(float a) 四舍五入
+    public static int max(int a,int b) 获取两个int值中的较大值
+    public static double pow(double a,double b) 返回a的b次幂的值
+    public static double random() 返回值为double的随机值，范围[0.0,1.0)
      */
 }
