d222a23d2afbfaf20eb2b5d09e85ef06f44cd02a
menjinyan <menjin728@gmail.com>
2025-03-10 18:01:14 +0800

    ParseInt方法的改进

---
+++ src/API/Integer/Demo05.java
@@ -0,0 +1,15 @@
+package src.API.Integer;
+
+import java.util.Scanner;
+
+public class Demo05 {
+    public static void main(String[] args) {
+        //键盘录入
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入一个字符串");
+        String line = sc.nextLine();
+        System.out.println(line);
+
+        //nextLine可以接受一整行数据
+    }
+}
